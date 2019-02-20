0171066856be5a062f12b311dbe0ce58fcad326d
fffrowies <fernandorowies.saxo@gmail.com>
2019-02-20 16:07:13 -0300

    Continue Login Activity

--- app/src/main/java/com/ffrowies/ecommercedemo/MainActivity.java
+++ app/src/main/java/com/ffrowies/ecommercedemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ffrowies.ecommercedemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
         btnMainJoinNow = (Button) findViewById(R.id.btn_main_join_now);
         btnMainLogin = (Button) findViewById(R.id.btn_main_login);
+
+        btnMainLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
