6d90c81b26a26fb02a89f8b33f4d8636572a2b67
fffrowies <fernandorowies.saxo@gmail.com>
2019-02-20 14:09:23 -0300

    Firebase db creation, Login Screen

--- app/src/main/java/com/ffrowies/ecommercedemo/MainActivity.java
+++ app/src/main/java/com/ffrowies/ecommercedemo/MainActivity.java
@@ -2,12 +2,18 @@ package com.ffrowies.ecommercedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnMainJoinNow, btnMainLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMainJoinNow = (Button) findViewById(R.id.btn_main_join_now);
+        btnMainLogin = (Button) findViewById(R.id.btn_main_login);
     }
 }
